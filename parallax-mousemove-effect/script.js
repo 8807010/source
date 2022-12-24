73b375dff6d09ca8a457313f127abbc0e99fea7d
Artiom_Sokolov <sokolova-net@mail.ru>
2022-12-24 14:34:15 +0300

    fix

--- parallax-mousemove-effect/script.js
+++ parallax-mousemove-effect/script.js
@@ -1,11 +1,16 @@
 document.addEventListener("mousemove", parallax);
 function parallax(e) {
   document.querySelectorAll(".object").forEach(function (move) {
+    // this.querySelectorAll(".object").forEach((move) => {
+    const moving_value = move.getAttribute("data-value");
 
-    let moving_value = move.getAttribute("data-value");
-    let x = (e.clientX * moving_value) / 250;
-    let y = (e.clientY * moving_value) / 250;
+    const x = (e.clientX * moving_value) / 250;
+    // const x = (window.innerWidth - e.pageX * moving_value) / 100;
+
+    const y = (e.clientY * moving_value) / 250;
+    // const y = (window.innerHeight - e.pageY * moving_value) / 100;
 
     move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    // move.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
 }
